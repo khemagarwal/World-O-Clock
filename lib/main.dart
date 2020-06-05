6f3a7ea472dfebcf73e34586d6d8f8fa3f83da5f
khedubaba <adityakhedekar49@gmail.com>
2020-06-05 13:15:50 +0530

    Routing added
    Signed-off-by: khedubaba <adityakhedekar49@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:worldoclock/pages/choose_location.dart';
 import 'package:worldoclock/pages/home.dart';
+import 'package:worldoclock/pages/loading.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+//    home: Home(),
+
+    initialRoute: "/home",
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      "/location": (context) => ChooseLocation(),
+    },
   ));
 }
