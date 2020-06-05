4ee0f70e40f6814466a8a784e781447e02037e9d
khedubaba <adityakhedekar49@gmail.com>
2020-06-05 11:05:32 +0530

    Initial Commit. Different page files clubbed into pages package.
    Signed-off-by: khedubaba <adityakhedekar49@gmail.com>

---
+++ android/app/src/main/java/com/khedubaba/worldoclock/MainActivity.java
@@ -0,0 +1,6 @@
+package com.khedubaba.worldoclock;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:worldoclock/pages/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
