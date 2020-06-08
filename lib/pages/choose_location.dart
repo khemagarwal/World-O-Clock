ab17fcd23ab71eb99ff68c17fc564d1f5df92005
khedubaba <adityakhedekar49@gmail.com>
2020-06-08 20:20:28 +0530

    ListView added for different locations along their respective country flags assets
    Signed-off-by: khedubaba <adityakhedekar49@gmail.com>

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:worldoclock/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   @override
@@ -6,6 +7,24 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  List<WorldTime> locations = [
+    WorldTime(url: 'Asia/Kolkata', location: 'Kolkata', flag: 'india.png'),
+    WorldTime(url: 'Asia/Tokyo', location: 'Tokyo', flag: 'japan.png'),
+    WorldTime(url: 'Australia/Sydney', location: 'Sydney', flag: 'australia.png'),
+    WorldTime(url: 'America/Vancouver', location: 'Vancouver', flag: 'canada.png'),
+    WorldTime(url: 'Pacific/Auckland', location: 'Auckland', flag: 'new-zealand.png'),
+    WorldTime(url: 'Europe/Madrid', location: 'Madrid', flag: 'spain.png'),
+    WorldTime(url: 'America/Recife', location: 'Recife', flag: 'brazil.png'),
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +36,25 @@ class _ChooseLocationState extends State<ChooseLocation> {
         title: Text("Choose a location"),
         centerTitle: true,
         elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index){
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+            child: Card(
+              child: ListTile(
+                onTap: () {
+                  print(locations[index].location);
+                },
+                title: Text(locations[index].location),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage('assets/${locations[index].flag }'),
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
